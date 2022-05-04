0780f9dad9d20214762adcf404e7cf7a5ada4bce
joao153 <joaovictorvieira153@gmail.com>
2022-05-04 20:41:24 -0300

    feat: create functions webs

---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+
+import { CloseButton } from "../CloseButton";
+
+import bugImageUrl from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import thoughtImageUrl from "../../assets/thought.svg";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStepe";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Imagem de uma lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Imagem de um balão de pensamento",
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+  // <FeedbackType | null> para ele receber o valor de null
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+    //essa função apenas limpa o estado de feedbackType
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSuccessStep onFeedbackRestartRequest={handleRestartFeedback} />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            //on  vai enviar para FeedbackTypeStep a variavel
+            //vai chamar uma função que está dentro de outro componente
+            //comunicação entre 2 componentes
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequest={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+
+      <footer className="text-xs ">
+        Feito com ♥ pela Rocketseat and{" "}
+        <a
+          className="underline underline-offset-1"
+          href="https://www.linkedin.com/in/joao-victor-vieira-b97556227/"
+        >
+          {" "}
+          JV-Vieira{" "}
+        </a>
+      </footer>
+    </div>
+  );
+}
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,10 +4,18 @@ module.exports = {
     extend: {
       colors:{
         brand:{
+          300: '#8257e6',
           500: '#8257e6',
         }
+      },
+      borderRadius:{
+        md:'4px'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+
+  ],
 }
